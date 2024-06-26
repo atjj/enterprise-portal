412413354d94344eafb73c36f8b91eacc263fde0
Erkebulan100 <67504870+Erkebulan100@users.noreply.github.com>
2024-06-26 11:42:18 +0600

    commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-module.exports {
-=======
 export default {
->>>>>>> 1123b42f4de022cbc6c69ee5f564803a048e0df5
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
--- src/pages/About/tailwind.config.js
+++ src/pages/About/tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
   },
+  extends: {},
   plugins: [],
 }
