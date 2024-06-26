bf7447b4d2d99b616eef723deb27a7aaa985a70a
Atay <atajmamasadykov@gmail.com>
2024-06-26 11:40:34 +0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <div className="h-[80px] bg-green-800 w-[1240px] mx-auto">
+        <div className="h-[80px] bg-green-800  w-full max-w-[1240px] mx-auto">
             <address className="text-center text-[17px] py-[20px] text-slate-50">
                 © 2024 ОсОО &quot;ШНОС&quot;
                 Кыргызская Республика, г. Бишкек
